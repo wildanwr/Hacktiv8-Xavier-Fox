8ca31b24298d24ad995079d5a70a30d31939fb03
Wildan Widya Rakhman <wildanwr@gmail.com>
2018-08-30 16:34:19 +0700

    Week2 Switch Case

---
+++ Phase0/Week2/Date(Switch-Case).js
@@ -0,0 +1,12 @@
+var tanggal = 1;
+var bulan = 5;
+var tahun = 1945;
+
+switch (tanggal && bulan && tahun) {
+  case 1 && 5 && 1945:
+    console.log('1 Mei 1945');
+    break;
+  default:
+    console.log('Put the current date in!');
+    break;
+}
