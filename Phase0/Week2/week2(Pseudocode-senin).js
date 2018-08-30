204475536f4a3cf51156c9e05dfcd0d16a4f2bfe
Wildan Widya Rakhman <wildanwr@gmail.com>
2018-08-30 16:15:12 +0700

    Add files via upload

---
+++ Phase0/Week2/week2(Pseudocode-senin).js
@@ -0,0 +1,53 @@
+/*
+1. Newton Second Law
+
+STORE "m" with 600 value
+STORE "a" with 2 value
+STORE "ΣF" with any value
+
+  CALCULATE "m" times "a"
+  SET "ΣF" value with calculation result
+
+DISPLAY "ΣF"
+*/
+
+
+/*
+2. Tahun Kabisat
+
+STORE "year" to any number
+
+IF "year" mod 4 is equals to 0 && mod 100 equals any number
+  DISPLAY "leap year"
+ELSE IF "year" mod 4 is equals to 0 && mod 100 equals to 0 && mod 400 equals to 0
+  DISPLAY "leap year"
+ELSE
+  DISPLAY "not leap year"
+*/
+
+
+/*
+3. Laundry Day
+
+STORE "wash clothes" with 0
+
+WHILE "wash clothes" <=20
+  ADD "wash clothes" by 1
+
+DISPLAY "washing time!"
+*/
+
+
+/*
+4. Periksa Kuku
+
+STORE  "students" to 0
+STORE "long nails"
+
+WHILE "students" <=40
+  ADD "students" by 1
+    IF "student" is "long nails"
+      DISPLAY "punished"
+    ELSE
+      DISPLAY "not punished"
+*/
