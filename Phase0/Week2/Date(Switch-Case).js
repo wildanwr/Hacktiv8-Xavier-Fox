4e3df6cce79834f9a65ccf657518d2d701ba951a
Wildan Widya Rakhman <wildanwr@gmail.com>
2018-09-02 23:14:55 +0700

    update switch case

--- Phase0/Week2/Date(Switch-Case).js
+++ Phase0/Week2/Date(Switch-Case).js
@@ -2,11 +2,59 @@ var tanggal = 1;
 var bulan = 5;
 var tahun = 1945;
 
-switch (tanggal && bulan && tahun) {
-  case 1 && 5 && 1945:
-    console.log('1 Mei 1945');
-    break;
-  default:
-    console.log('Put the current date in!');
-    break;
+
+switch(tanggal) {
+  case 1: console.log(1); break;
+  case 2: console.log(2); break;
+  case 3: console.log(3); break;
+  case 4: console.log(4); break;
+  case 5: console.log(5); break;
+  case 6: console.log(6); break;
+  case 7: console.log(7); break;
+  case 8: console.log(8); break;
+  case 9: console.log(9); break;
+  case 10: console.log(10); break;
+  case 11: console.log(11); break;
+  case 12: console.log(12); break;
+  case 13: console.log(13); break;
+  case 14: console.log(14); break;
+  case 15: console.log(15); break;
+  case 16: console.log(16); break;
+  case 17: console.log(17); break;
+  case 18: console.log(18); break;
+  case 19: console.log(19); break;
+  case 20: console.log(20); break;
+  case 21: console.log(21); break;
+  case 22: console.log(22); break;
+  case 23: console.log(23); break;
+  case 24: console.log(24); break;
+  case 25: console.log(25); break;
+  case 26: console.log(26); break;
+  case 27: console.log(27); break;
+  case 28: console.log(28); break;
+  case 29: console.log(29); break;
+  case 30: console.log(30); break;
+  case 31: console.log(31); break;
+  default: console.log('Masukkan tanggal dengan benar!')
+}
+
+switch(bulan) {
+  case 1: console.log('Januari'); break;
+  case 2: console.log('Februari'); break;
+  case 3: console.log('Maret'); break;
+  case 4: console.log('April'); break;
+  case 5: console.log('Mei'); break;
+  case 6: console.log('Juni'); break;
+  case 7: console.log('Juli'); break;
+  case 8: console.log('Agustus'); break;
+  case 9: console.log('September'); break;
+  case 10: console.log('Oktober'); break;
+  case 11: console.log('November'); break;
+  case 12: console.log('Desember'); break;
+  default: console.log('Masukkan bulan dengan benar!')
+}
+
+switch(true) {
+  case (tahun >= 1900 && tahun <= 2200): console.log(tahun); break;
+  default: console.log('Masukkan tahun dengan benar!')
 }
