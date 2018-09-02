b0d09dba408d2a76fcaa28bdfb1d47ff912deb40
Wildan Widya Rakhman <wildanwr@gmail.com>
2018-09-02 16:07:15 +0700

    Update Proxytia

--- Phase0/Week2/Proxytia.js
+++ Phase0/Week2/Proxytia.js
@@ -1,17 +1,17 @@
-var nama = 'Wildan';
-var peran = 'Ksatria';
+var nama = ''
+var peran = ''
 
-if (nama === 'Wildan' && peran === '') {
-  console.log("Halo Wildan, Pilih peranmu untuk memulai game!");
-} else if (nama === 'Wildan' && peran === 'Ksatria') {
-  console.log("Selamat datang di Dunia Proxytia Wildan")
-  console.log("Halo Ksatria Wildan, kamu dapat menyerang dengan senjatamu!");
-} else if (nama === 'Wildan' && peran === 'Tabib') {
-  console.log("Selamat datang di Dunia Proxytia Wildan")
-  console.log("Halo Tabib Wildan, kamu akan membantu temanmu yang terluka.");
-} else if (nama === 'Wildan' && peran === 'Penyihir') {
-  console.log("Selamat datang di Dunia Proxytia Wildan")
-  console.log("Halo Penyihir Wildan, ciptakan keajaiban yang membantu kemenanganmu!");
+if (nama !== '' && peran === '') {
+    console.log('Halo ' + nama + ' Pilih peranmu untuk memulai game!')
+} else if (nama !== '' && peran === 'Ksatria') {
+    console.log('Selamat datang di Dunia Proxytia ' + nama)
+    console.log('Halo Ksatria ' + nama + ' kamu dapat menyerang dengan senjatamu!')
+} else if (nama !== '' && peran === 'Tabib') {
+    console.log('Selamat datang di Dunia Proxytia ' + nama)
+    console.log('Halo Tabib ' + nama + ' kamu akan membantu temanmu yang terluka.')
+} else if (nama ==! '' && peran === 'Penyihir') {
+    console.log('Selamat datang di Dunia Proxytia ' + nama)
+    console.log('Halo Penyihir ' + nama + ' ciptakan keajaiban yang membantu kemenanganmu!')
 } else {
-  console.log("Nama harus diisi!");
+    console.log('Nama harus diisi!')
 }
