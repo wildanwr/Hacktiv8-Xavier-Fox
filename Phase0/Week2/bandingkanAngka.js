d54f744f9ed295edc18f0c18063069f68cf69cf5
Wildan Widya Rakhman <wildanwr@gmail.com>
2018-09-02 16:16:46 +0700

    create bandingkan angka week 2

---
+++ Phase0/Week2/bandingkanAngka.js
@@ -0,0 +1,18 @@
+function bandingkanAngka(angka1, angka2) {
+  // you can only write your code here!
+  if (angka2 > angka1) {
+    return console.log('true')
+  } else if (angka2 < angka1) {
+    return console.log (false)
+  } else {
+    return console.log(-1)
+  }
+}
+
+/* TEST CASES
+console.log(bandingkanAngka(5, 8)); // true
+console.log(bandingkanAngka(5, 3)); // false
+console.log(bandingkanAngka(4, 4)); // -1
+console.log(bandingkanAngka(3, 3)); // -1
+console.log(bandingkanAngka(17, 2)); // false
+*/
